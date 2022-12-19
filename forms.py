6e7f96d12e1213f6251387aaf00d564084ca42bf
t-ega <41376025+t-ega@users.noreply.github.com>
2022-12-19 14:31:23 +0100

    Delete forms.py

--- forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-
-class ImageForm(forms.Form):
-    image = forms.ImageField(required=True)
-    description = forms.CharField(max_length=256, label='What is this image about?')
